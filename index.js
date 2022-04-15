a9f720d5798b16ea6a487a29bf516fdc69e2932e
Luiz Medina <luizmedina87@gmail.com>
2022-04-15 16:14:01 -0400

    feature questions prompt

--- index.js
+++ index.js
@@ -1,13 +1,115 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+// const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project? (Required)",
+    validate: (titleInput) => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log("Please enter a title for your project!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter your project's description. (Required)",
+    validate: (titleInput) => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log("Please enter a description for your project!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "authors",
+    message: "Enter the author's names. (Required)",
+    validate: (titleInput) => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log("Please enter the name of the users!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your github username. (Required)",
+    validate: (titleInput) => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log("Please enter your github username!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How to install your project? (Optional)",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter the usage content. (Optional)",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What license do you want to use for your project? (Optional)",
+    choices: [
+      "EPL-2.0",
+      "CDDL-1.0",
+      "MPL-2.0",
+      "MIT",
+      "lgpl-license",
+      "gpl-license",
+      "BSD-2-Clause",
+      "BSD-3-Clause",
+      "Apache-2.0",
+      "None",
+    ],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Enter the contributing section. (Optional)",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Enter tests to run. (Optional)",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+const init = (questions) => {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      console.log(answers);
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        console.log("Prompt couldn't be rendered in the current environment");
+      } else {
+        console.log("Something else went wrong");
+      }
+    });
+};
 
-// Function call to initialize app
-init();
+init(questions);
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,40 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `[![License](https://img.shields.io/badge/License-${license}-blue.svg)]`;
+  }
+  return ""; 
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return `(https://opensource.org/licenses/${license})`
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return `
+    ## LICENSE
+    ${renderLicenseLink(license)}
+    `
+  }
+  return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(license)}
 
-`;
+  ${renderLicenseSection(data.license)}
+  `;
 }
 
 module.exports = generateMarkdown;
