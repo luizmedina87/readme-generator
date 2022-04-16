21b16b95d93b47683c8d2715c98772b4e5a0d34d
Luiz Medina <luizmedina87@gmail.com>
2022-04-15 23:04:10 -0400

    creates readme file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// TODO: Include packages needed for this application
 const inquirer = require("inquirer");
-// const generateMarkdown = require("./utils/generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
 
 const questions = [
   {
@@ -17,6 +17,23 @@ const questions = [
     },
   },
   {
+    type: "list",
+    name: "license",
+    message: "What license do you want to use for your project? (Optional)",
+    choices: [
+      "EPL-2.0",
+      "CDDL-1.0",
+      "MPL-2.0",
+      "MIT",
+      "lgpl-license",
+      "gpl-license",
+      "BSD-2-Clause",
+      "BSD-3-Clause",
+      "Apache-2.0",
+      "None",
+    ],
+  },
+  {
     type: "input",
     name: "description",
     message: "Enter your project's description. (Required)",
@@ -31,8 +48,8 @@ const questions = [
   },
   {
     type: "input",
-    name: "authors",
-    message: "Enter the author's names. (Required)",
+    name: "author",
+    message: "Enter the author's name. (Required)",
     validate: (titleInput) => {
       if (titleInput) {
         return true;
@@ -57,30 +74,18 @@ const questions = [
   },
   {
     type: "input",
+    name: "demo",
+    message: "Input the project demo path? (Optional)",
+  },
+  {
+    type: "input",
     name: "installation",
     message: "How to install your project? (Optional)",
   },
   {
     type: "input",
     name: "usage",
-    message: "Enter the usage content. (Optional)",
-  },
-  {
-    type: "list",
-    name: "license",
-    message: "What license do you want to use for your project? (Optional)",
-    choices: [
-      "EPL-2.0",
-      "CDDL-1.0",
-      "MPL-2.0",
-      "MIT",
-      "lgpl-license",
-      "gpl-license",
-      "BSD-2-Clause",
-      "BSD-3-Clause",
-      "Apache-2.0",
-      "None",
-    ],
+    message: "Enter the usage information. (Optional)",
   },
   {
     type: "input",
@@ -94,22 +99,30 @@ const questions = [
   },
 ];
 
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+
+function writeToFile(data) {
+  fs.writeFile(`./dist/README.md`, data, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  console.log("File created in 'dist' folder!")
+}
 
 const init = (questions) => {
-  inquirer
-    .prompt(questions)
-    .then((answers) => {
-      console.log(answers);
-    })
-    .catch((error) => {
-      if (error.isTtyError) {
-        console.log("Prompt couldn't be rendered in the current environment");
-      } else {
-        console.log("Something else went wrong");
-      }
-    });
+  return inquirer.prompt(questions).catch((error) => {
+    if (error.isTtyError) {
+      console.log("Prompt couldn't be rendered in the current environment");
+    } else {
+      console.log("Something else went wrong");
+    }
+  });
 };
 
-init(questions);
+init(questions)
+  .then((res) => {
+    return generateMarkdown(res);
+  })
+  .then((res) => {
+    writeToFile(res);
+  });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,40 +1,131 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== "None") {
-    return `[![License](https://img.shields.io/badge/License-${license}-blue.svg)]`;
+    return `![License](https://img.shields.io/badge/License-${license.replace(
+      "-",
+      "%20"
+    )}-blue.svg)`;
   }
-  return ""; 
+  return "";
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license !== "None") {
-    return `(https://opensource.org/licenses/${license})`
+    return `https://opensource.org/licenses/${license}`;
   }
   return "";
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license !== "None") {
-    return `
-    ## LICENSE
-    ${renderLicenseLink(license)}
-    `
+    return `## License\n${renderLicenseLink(license)}\n\n`;
   }
   return "";
 }
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-  ${renderLicenseBadge(license)}
+function renderDemoSection(git, title, path) {
+  if (path) {
+    return `## Demo\n![project demo](https://github.com/${git}/${title}/blob/main/${path})\n\n`;
+  }
+  return "";
+}
 
-  ${renderLicenseSection(data.license)}
-  `;
+function renderInstallSection(instructions) {
+  if (instructions) {
+    return `## Install\n${instructions}\n\n`;
+  }
+  return "";
+}
+
+function renderUsageSection(usage) {
+  if (usage) {
+    return `## Usage\n${usage}\n\n`;
+  }
+  return "";
+}
+
+function renderContributingSection(contributing) {
+  if (contributing) {
+    return `## Contributing\n${contributing}\n\n`;
+  }
+  return "";
+}
+
+function renderTestsSection(tests) {
+  if (tests) {
+    return `## Tests\n${tests}\n\n`;
+  }
+  return "";
+}
+
+function demoLink(info) {
+  if (info) {
+    return `* [Demo](#demo)\n`;
+  }
+  return "";
+}
+
+function installLink(info) {
+  if (info) {
+    return `* [Install](#install)\n`;
+  }
+  return "";
+}
+
+function usagelLink(info) {
+  if (info) {
+    return `* [Usage](#usage)\n`;
+  }
+  return "";
+}
+
+function contributingLink(info) {
+  if (info) {
+    return `* [Contributing](#contributing)\n`;
+  }
+  return "";
+}
+
+function testsLink(info) {
+  if (info) {
+    return `* [Tests](#tests)\n`;
+  }
+  return "";
+}
+
+function licenseLink(info) {
+  if (info) {
+    return `* [License](#license)\n`;
+  }
+  return "";
+}
+
+function generateMarkdown(data) {
+  return (
+    `# ${data.title}\n` +
+    `${renderLicenseBadge(data.license)}\n\n` +
+    `## Table of contents\n` +
+    `* [Description](#description)\n` +
+    `* [Author](#author)\n` +
+    `* [Questions](#questions)\n` +
+    `${demoLink(data.demo)}` +
+    `${installLink(data.installation)}` +
+    `${usagelLink(data.usage)}` +
+    `${contributingLink(data.contributing)}` +
+    `${testsLink(data.tests)}` +
+    `${licenseLink(data.license)}` +
+    `\n## Description\n` +
+    `${data.description}\n\n` +
+    `## Author\n` +
+    `${data.author}\n\n` +
+    `## Questions\n` +
+    `https://github.com/${data.github}\n\n` +
+    `${renderDemoSection(data.github, data.title, data.demo)}` +
+    `${renderInstallSection(data.installation)}` +
+    `${renderUsageSection(data.usage)}` +
+    `${renderContributingSection(data.contributing)}` +
+    `${renderTestsSection(data.tests)}` +
+    `${renderLicenseSection(data.license)}`
+  );
 }
 
 module.exports = generateMarkdown;
